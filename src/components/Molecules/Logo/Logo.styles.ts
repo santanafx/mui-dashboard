7a748380b6308ef0f3ebc9773ec9b2210458083c
lucas <santanafx@hotmail.com>
2024-08-20 14:20:33 -0300

    feat: created logo styles

---
+++ src/components/Molecules/Logo/Logo.styles.ts
@@ -0,0 +1,16 @@
+import { styled } from '@mui/system';
+
+export const DivLogoContainer = styled('div')(() => ({
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+}));
+
+export const DivImageContainer = styled('div')(() => ({
+  width: '40%',
+}));
+
+export const ImgLogo = styled('img')(() => ({
+  maxWidth: '100%',
+}));
