43b7c2a329c56cb6b09c64ac3146c95d590f86f6
lucas <santanafx@hotmail.com>
2024-08-21 18:35:17 -0300

    feat: added zod schema and error messages to inputs

--- src/components/Molecules/NutritionModal/NutritionModal.tsx
+++ src/components/Molecules/NutritionModal/NutritionModal.tsx
@@ -1,11 +1,29 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Button, ModalClose, Typography } from '@mui/joy';
 import { Controller, useForm } from 'react-hook-form';
+import * as z from 'zod';
 
 import {
   FoodModalProps,
   NutritionModalProps,
 } from './NutritionModal.interface';
-import { DivButtons, DivForm, InputNutrition } from './NutritionModal.style';
+import {
+  DivButtons,
+  DivForm,
+  DivFormButtons,
+  InputNutrition,
+  TypographyError,
+} from './NutritionModal.style';
+
+const nutritionSchema = z.object({
+  name: z.string().min(1, { message: 'Name is required' }),
+  calories: z.string().min(1, { message: 'Calories is required' }),
+  fat: z.string().min(1, { message: 'Fat is required' }),
+  carbs: z.string().min(1, { message: 'Carbs is required' }),
+  protein: z.string().min(1, { message: 'Protein is required' }),
+});
+
+type nutritionSchemaType = z.infer<typeof nutritionSchema>;
 
 export const NutritionModal = ({
   open,
@@ -13,7 +31,8 @@ export const NutritionModal = ({
   foodTable,
   setFoodTable,
 }: NutritionModalProps) => {
-  const { handleSubmit, control } = useForm({
+  const { handleSubmit, control } = useForm<nutritionSchemaType>({
+    resolver: zodResolver(nutritionSchema),
     defaultValues: {
       name: '',
       calories: '',
@@ -51,54 +70,79 @@ export const NutritionModal = ({
       >
         Add item
       </Typography>
-      <DivForm>
-        <Typography>Name</Typography>
-        <Controller
-          control={control}
-          name="name"
-          render={({ field }) => (
-            <InputNutrition placeholder="Name" type="text" {...field} />
-          )}
-        />
-        Calories
-        <Controller
-          control={control}
-          name="calories"
-          render={({ field }) => (
-            <InputNutrition placeholder="Calories" type="number" {...field} />
-          )}
-        />
-        <Typography>Fat</Typography>
-        <Controller
-          control={control}
-          name="fat"
-          render={({ field }) => (
-            <InputNutrition placeholder="Fat" type="number" {...field} />
-          )}
-        />
-        <Typography>Carbs</Typography>
-        <Controller
-          control={control}
-          name="carbs"
-          render={({ field }) => (
-            <InputNutrition placeholder="Carbs" type="number" {...field} />
-          )}
-        />
-        <Typography>Protein</Typography>
-        <Controller
-          control={control}
-          name="protein"
-          render={({ field }) => (
-            <InputNutrition placeholder="Protein" type="number" {...field} />
-          )}
-        />
-      </DivForm>
-      <DivButtons>
-        <Button variant="soft" onClick={() => setOpen(!open)}>
-          Cancel
-        </Button>
-        <Button onClick={handleSubmit(handleNutritionForm)}>Save</Button>
-      </DivButtons>
+      <DivFormButtons>
+        <DivForm>
+          <Typography>Name</Typography>
+          <Controller
+            control={control}
+            name="name"
+            render={({ field, fieldState: { error } }) => (
+              <div>
+                <InputNutrition placeholder="Name" type="text" {...field} />
+                {error && <TypographyError>{error.message}</TypographyError>}
+              </div>
+            )}
+          />
+          <Typography>Calories</Typography>
+          <Controller
+            control={control}
+            name="calories"
+            render={({ field, fieldState: { error } }) => (
+              <div>
+                <InputNutrition
+                  placeholder="Calories"
+                  type="number"
+                  {...field}
+                />
+                {error && <TypographyError>{error.message}</TypographyError>}
+              </div>
+            )}
+          />
+          <Typography>Fat</Typography>
+          <Controller
+            control={control}
+            name="fat"
+            render={({ field, fieldState: { error } }) => (
+              <div>
+                <InputNutrition placeholder="Fat" type="number" {...field} />
+                {error && <TypographyError>{error.message}</TypographyError>}
+              </div>
+            )}
+          />
+          <Typography>Carbs</Typography>
+          <Controller
+            control={control}
+            name="carbs"
+            render={({ field, fieldState: { error } }) => (
+              <div>
+                <InputNutrition placeholder="Carbs" type="number" {...field} />
+                {error && <TypographyError>{error.message}</TypographyError>}
+              </div>
+            )}
+          />
+          <Typography>Protein</Typography>
+          <Controller
+            control={control}
+            name="protein"
+            render={({ field, fieldState: { error } }) => (
+              <div>
+                <InputNutrition
+                  placeholder="Protein"
+                  type="number"
+                  {...field}
+                />
+                {error && <TypographyError>{error.message}</TypographyError>}
+              </div>
+            )}
+          />
+        </DivForm>
+        <DivButtons>
+          <Button variant="soft" onClick={() => setOpen(!open)}>
+            Cancel
+          </Button>
+          <Button onClick={handleSubmit(handleNutritionForm)}>Save</Button>
+        </DivButtons>
+      </DivFormButtons>
     </>
   );
 };
