a4207f40ab535e4a853c19c68466888c1be90b47
lucas <santanafx@hotmail.com>
2024-09-01 15:47:04 -0300

    feat: new delete modal file

---
+++ src/components/Molecules/DeleteNutritionModal/DeleteNutritionModal.tsx
@@ -0,0 +1,43 @@
+import { Button, ModalClose, Typography } from '@mui/joy';
+
+import { deleteFood } from '../../../Services/CardTable/CardTable.service';
+import { DeleteNutritionModalProps } from './DeleteNutritionModal.interface';
+import { DivButtons, DivContainer } from './DeleteNutritionModal.styles';
+
+export const DeleteNutritionModal = ({
+  open,
+  setOpen,
+  selectTableItem,
+}: DeleteNutritionModalProps) => {
+  const { name } = selectTableItem;
+
+  const handleDeleteItem = () => {
+    deleteFood(selectTableItem.id!);
+    setOpen(!open);
+  };
+
+  return (
+    <>
+      <ModalClose variant="plain" sx={{ m: 1 }} />
+      <Typography
+        component="h2"
+        id="modal-title"
+        level="h4"
+        textColor="inherit"
+        fontWeight="lg"
+        mb={1}
+      >
+        Delete item
+      </Typography>
+      <DivContainer>
+        <Typography>{`Are you sure you want to delete ${name}?`}</Typography>
+        <DivButtons>
+          <Button variant="soft" onClick={() => setOpen(!open)}>
+            Cancel
+          </Button>
+          <Button onClick={() => handleDeleteItem()}>Yes</Button>
+        </DivButtons>
+      </DivContainer>
+    </>
+  );
+};
