05ce6c958dee3b98f038d98c190210992c10627a
lucas <santanafx@hotmail.com>
2024-09-22 09:45:55 -0300

    feat: new div

--- src/components/Molecules/DeleteNutritionModal/DeleteNutritionModal.tsx
+++ src/components/Molecules/DeleteNutritionModal/DeleteNutritionModal.tsx
@@ -2,7 +2,11 @@ import { Button, ModalClose, Typography } from '@mui/joy';
 
 import { deleteFood } from '../../../Services/CardTable/CardTable.service';
 import { DeleteNutritionModalProps } from './DeleteNutritionModal.interface';
-import { DivButtons, DivContainer } from './DeleteNutritionModal.styles';
+import {
+  DivButtons,
+  DivContainer,
+  DivTypography,
+} from './DeleteNutritionModal.styles';
 
 export const DeleteNutritionModal = ({
   open,
@@ -19,16 +23,18 @@ export const DeleteNutritionModal = ({
   return (
     <>
       <ModalClose variant="plain" sx={{ m: 1 }} />
-      <Typography
-        component="h2"
-        id="modal-title"
-        level="h4"
-        textColor="inherit"
-        fontWeight="lg"
-        mb={1}
-      >
-        Delete item
-      </Typography>
+      <DivTypography>
+        <Typography
+          component="h2"
+          id="modal-title"
+          level="h4"
+          textColor="inherit"
+          fontWeight="lg"
+          mb={1}
+        >
+          Delete item
+        </Typography>
+      </DivTypography>
       <DivContainer>
         <Typography>{`Are you sure you want to delete ${name}?`}</Typography>
         <DivButtons>
