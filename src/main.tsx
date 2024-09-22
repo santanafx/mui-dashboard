1e5ad5bdbfb63caf78b0bbed8b71e2f156c039f4
lucas <santanafx@hotmail.com>
2024-09-22 09:45:35 -0300

    refactor: removed login route

--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,11 @@ import './Global.css';
 import { ThemeProvider } from '@mui/joy';
 
 import { Home } from './Pages/Home/Home.tsx';
-import { Login } from './Pages/Login/Login.tsx';
 import { theme } from './Theme.ts';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Login />,
-  },
-  {
-    path: '/home',
     element: <Home />,
   },
 ]);
